7362937c71d585180af8677fe03b80563ac67358
jacob-flores <jacobflores1@gmail.com>
2018-10-09 00:15:31 -0500

    Exercises for Section 18. Const/Let

---
+++ Section_8/CodingExercise_18.js
@@ -0,0 +1,36 @@
+/*
+Const/Let Refactoring
+
+The following code uses 'var' instead of 'const' and 'let'. Refactor the function to use the new keywords. 
+Be sure to consider whether the variable should be declared using 'const' or 'let' depending on whether
+the variable gets reassigned or not.
+
+var statuses = [ 
+  { code: 'OK', response: 'Request successful' },
+  { code: 'FAILED', response: 'There was an error with your request' },
+  { code: 'PENDING', response: 'Your reqeust is still pending' }
+];
+var message = '';
+var currentCode = 'OK';
+
+for (var i = 0; i < statuses.length; i++) {
+  if (statuses[i].code === currentCode) {
+    message = statuses[i].response;
+  }
+}
+
+*/
+
+const statuses = [ 
+    { code: 'OK', response: 'Request successful' },
+    { code: 'FAILED', response: 'There was an error with your request' },
+    { code: 'PENDING', response: 'Your reqeust is still pending' }
+  ];
+  let message = '';
+  let currentCode = 'OK';
+  
+  for (let i = 0; i < statuses.length; i++) {
+    if (statuses[i].code === currentCode) {
+      message = statuses[i].response;
+    }
+  }

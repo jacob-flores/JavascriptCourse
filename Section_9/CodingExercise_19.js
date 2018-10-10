db42367664f504d753bbdfc08508c229621d53e5
jacob-flores <jacobflores1@gmail.com>
2018-10-09 23:16:22 -0500

    Exercises for section 09: Template Strings

---
+++ Section_9/CodingExercise_19.js
@@ -0,0 +1,15 @@
+/* 
+Template Strings in Practice
+
+Refactor the function to use template strings
+
+function doubleMessage(number) {
+  return "Your number doubled is " + (2 * number);
+}
+*/
+
+function doubleMessage(number) {
+    return `Your number doubled is ${2 * number}`;
+  }
+
+  console.log(doubleMessage(11));
---
+++ Section_9/CodingExercise_20.js
@@ -0,0 +1,15 @@
+/* 
+Name Helpers
+Refactor the function to use template strings
+
+function fullName(firstName, lastName) {
+  return firstName + lastName;
+}
+
+*/
+
+function fullName(firstName, lastName) {
+     return `${firstName} ${lastName}`;
+  }
+
+  console.log(fullName('Jacob', 'Flores'));
